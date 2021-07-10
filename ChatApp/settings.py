353ce5b45a8236ffee4a31e3ae8e89de6e7a24a2
אביב בר-און <avivbaron7@gmail.com>
2021-07-10 17:34:53 +0300

    updated allowed-hosts

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#nv&7=my(8drxahj9-1-_rdmvy8n)$#cdzg0ty1$-aa=unw+$4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://whispering-eyrie-10695.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
