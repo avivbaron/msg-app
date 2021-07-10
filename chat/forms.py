74fdfebebe3c429e76352fb9faa12924f7d9dba5
אביב בר-און <avivbaron7@gmail.com>
2021-07-11 00:11:23 +0300

    update beautify

--- chat/forms.py
+++ chat/forms.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class SignUpForm(UserCreationForm):
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    
     class Meta:
         model = User
         fields=('username','password1','password2')
--- chat/views.py
+++ chat/views.py
@@ -7,21 +7,27 @@ from .models import Message
 from .forms import SignUpForm
 import json
 
+
 @csrf_exempt
 def do_login(request):
+
     if request.user.is_authenticated:
         return HttpResponse('{"user is connected"}')
+
     if request.method == "POST":
         username, password = request.POST['username'], request.POST['password']
         user = authenticate(username=username, password=password)
+
         if user is not None:
             login(request, user)
         else:
             return HttpResponse('{"error": "User does not exist"}')
         return HttpResponse('{"user is connected"}')
 
+
 @csrf_exempt
 def register_view(request):
+
     if request.method == 'POST':
         form = SignUpForm(request.POST)
 
@@ -29,25 +35,29 @@ def register_view(request):
             user = form.save(commit=False)
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
+            
             user.set_password(password)
             user.save()
             user = authenticate(username=username, password=password)
+
             if user is not None:
                 if user.is_active:
                     login(request, user)
                     return HttpResponse('{"registered"}')
+
     return HttpResponse('{"not registered"}')
 
 
-
 @csrf_exempt
-def get_all_messages(request, user_id=None):
-    """
-    List all required messages.
-    """
+def get_all_messages(request, user_id):
+
     if request.method == 'GET':
+        if user_id is None:
+            return HttpResponse('user_id is not specified')
+
         messages = (Message.objects.filter(receiver_id=user_id) | Message.objects.filter(sender_id=user_id))
         response_json = {}
+
         for message in messages:
             message.is_read = True
             message.save()
@@ -56,11 +66,13 @@ def get_all_messages(request, user_id=None):
                 'sender_id': message.sender_id,
                 'message_content': message.message,
             }
+
         return HttpResponse(json.dumps(response_json), content_type="application/json")
 
 
 @csrf_exempt
 def get_signle_message(request, message_id):
+
     if request.method == 'GET':
         try:
             message = Message.objects.get(id=message_id)
@@ -80,9 +92,11 @@ def get_signle_message(request, message_id):
 
 @csrf_exempt
 def get_all_unread_message(request, user_id):
+
     if request.method == 'GET':
         messages = Message.objects.filter(receiver_id=user_id, is_read = False)
         response_json = {}
+
         for message in messages:
            message.is_read = True
            message.save()
@@ -95,7 +109,8 @@ def get_all_unread_message(request, user_id):
     
 
 @csrf_exempt
-def send_message(request):    
+def send_message(request):  
+
     if request.method == 'POST':
         data = JSONParser().parse(request)
         receiver = User.objects.filter(username=data['receiver']).first()
@@ -107,6 +122,7 @@ def send_message(request):
 
 @csrf_exempt
 def message_delete(request, message_id, user_id):
+
      if request.method == 'DELETE':
         try:
             message = Message.objects.get(id=message_id)
@@ -116,6 +132,7 @@ def message_delete(request, message_id, user_id):
         if user_id in [message.receiver_id, message.sender_id]:
             message.delete()
             return HttpResponse({"message has deleted"})
+
         return HttpResponse({"message has not been deleted due to user_id mismatch"})
 
 
